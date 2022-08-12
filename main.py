c84e910cd88b7f6b4a080edb451460e9fc5bcee6
jasmineteters <jasmineteters2020@gmail.com>
2022-08-11 21:06:44 -0700

    Added ball functionality and scoreboard tracking.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,17 +12,36 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
-
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
-
+ball.move()
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 60 and ball.xcor() > 320 or ball.distance(l_paddle) < 60 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+    if scoreboard.game_over():
+        game_is_on = False
 
 screen.exitonclick()
